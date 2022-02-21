36a71b539091e339f72591f753be73f95a4a6ba9
David Lang <david.lang.ca@outlook.com>
2022-02-21 12:44:53 -0500

    update

--- Himeji-WebApi/Program.cs
+++ Himeji-WebApi/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var podName = Environment.GetEnvironmentVariable("HOSTNAME");
+if (string.IsNullOrEmpty(podName)) podName = "Unable to read HOSTNAME";
 if (!string.IsNullOrEmpty(podName))
 {
     Himeji.Framework.Slack.Api.WebHookBotDuty.SendMessage($"Himeji App Initializing on pod: {podName}");
