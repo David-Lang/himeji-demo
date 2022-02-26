a8e7a1d882f966305436029ef940558134351e51
David Lang <david.lang.ca@outlook.com>
2022-02-26 18:23:33 -0500

    update

---
+++ Himeji.Kubernetes/namespace-prep-conjour.sh
@@ -0,0 +1,6 @@
+﻿
+
+
+helm install namespace-prep cyberark/conjur-config-namespace-prep -n "himejijo" \
+  --set authnK8s.goldenConfigMap="conjur-configmap" \
+  --set authnK8s.namespace="conjur-helm-aks"
