5173059b200e22433585c16ef0685199bf0d77e3
David Lang <david.lang.ca@outlook.com>
2022-02-28 16:55:37 -0500

    update

---
+++ Himeji.Kubernetes/Matsumoto-aks/8-helm-install-app.sh
@@ -0,0 +1,6 @@
+﻿
+git clone https://github.com/David-Lang/himeji-demo.git
+
+helm install himeji-app himeji-demo/Himeji-WebApi/Helm-Charts/release-0.0.0/himeji-app/ -n app-demo --debug
+
+helm upgrade himeji-app himeji-demo/Himeji-WebApi/Helm-Charts/release-0.1.0/himeji-app/ -n app-demo --debug
