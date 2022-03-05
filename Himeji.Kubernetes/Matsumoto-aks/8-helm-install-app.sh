88d7902000fbaa0ee1d1c443bce46f3664f6af13
David Lang <david.lang.ca@outlook.com>
2022-03-05 14:13:20 -0500

    update

--- Himeji.Kubernetes/Matsumoto-aks/8-helm-install-app.sh
+++
@@ -1,6 +0,0 @@
-﻿
-git clone https://github.com/David-Lang/himeji-demo.git
-
-helm install himeji-app himeji-demo/Himeji-WebApi/Helm-Charts/release-0.0.0/himeji-app/ -n app-demo --debug
-
-helm upgrade himeji-app himeji-demo/Himeji-WebApi/Helm-Charts/release-0.1.0/himeji-app/ -n app-demo --debug
--- Himeji.Kubernetes/namespace-prep-conjour.sh
+++
@@ -1,6 +0,0 @@
-﻿
-
-
-helm install namespace-prep cyberark/conjur-config-namespace-prep -n "himejijo" \
-  --set authnK8s.goldenConfigMap="conjur-configmap" \
-  --set authnK8s.namespace="conjur-helm-aks"
