a5a49db7f59b3b52ee490467ee29d5e19a7a9b51
David Lang <david.lang.ca@outlook.com>
2022-03-05 19:30:43 -0500

    update

--- Himeji.Kubernetes/Matsumoto-aks/9-bootstrap-install-flux.sh
+++ Himeji.Kubernetes/Matsumoto-aks/9-bootstrap-install-flux.sh
@@ -2,7 +2,7 @@ export GITHUB_TOKEN="$(cat /opt/himeji/secrets/github-token)"
 
 flux bootstrap github \
   --owner=david-lang \
-  --repository=flux-aks \
+  --repository=flux-demo \
   --branch=master \
   --path=./clusters/my-cluster \
   --personal
